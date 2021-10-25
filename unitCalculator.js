118ec5ce7a4c56bb5d21655a0fefdddb0befc93a
Karolina K <kaczmarska.kar@gmail.com>
2021-10-25 18:21:05 +0100

    some refactoring

--- unitCalculator.js
+++ unitCalculator.js
@@ -1,5 +1,7 @@
 const prompt = require('prompt-sync')();
 
+
+const arrayOfUnits = ['m', 'inch', 'kg', 'lbs', 'km', 'mi']
 const mToInch = (value) => value * 39.37
 const kmToMiles = (value) => value/1.609
 const kgToPounds = (value) => value * 2.205
@@ -7,20 +9,26 @@ const kgToPounds = (value) => value * 2.205
 let initialUnitInput = prompt('insert initial unit: ')
 let resultUnitInput = prompt('insert result unit: ')
 let valueInput = prompt('insert value which you want to calculate: ')
-valueInput = Number(valueInput)
+valueInput = Number(valueInput);
 
-const unitConversion = (initialUnit = '', resultUnit = '', value= 0) => {
 
-    if(initialUnit === '' || resultUnit === '' || value === 0) {throw 'You must provide details for your calculation'}
+const unitConversion = (initialUnit, resultUnit, value) => {
+
+    const isValidUnit = arrayOfUnits.some(unit => (initialUnit || resultUnit) !== unit);
+    const hasValues = initialUnit === '' || resultUnit === '' || value === 0;
+
+    if(isValidUnit) { throw 'You must provide valid unit represantion' };
+    if(hasValues) { throw 'You must provide details for your calculation' }; 
+
     if(initialUnit === 'kg' && resultUnit === 'lbs') {
         return kgToPounds(value)
-    }
+    };
     if(initialUnit === 'km' && resultUnit === 'mi') {
         return kmToMiles(value)
-    }
+    };
     if(initialUnit === 'm' && resultUnit === 'inch') {
         return mToInch(value)
-    }
+    };
 return
 }
 console.log(unitConversion(initialUnitInput, resultUnitInput, valueInput)) 
